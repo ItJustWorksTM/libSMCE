f5b3b0c2163fa5a2537bb793a88e1b950084dcc3
Erik Tran <35762899+eriktran@users.noreply.github.com>
2021-09-21 17:46:17 +0200

    Update minimum-cmake-version.sh
    Update autoinstall and update cmake
    
    Co-Authored-By: Clementine Jensen <gusjensecl@student.gu.se>

--- ci/minimum-cmake-version.sh
+++ ci/minimum-cmake-version.sh
@@ -5,10 +5,6 @@ MINIMUM_VERSION=3.12.0
 INSTALLED_VERSION_ARRAY=($INSTALLED_VERSION)
 
 CMAKE_LOCATION=$(where cmake)
-CHOCO_LOCATION=$(where choco)
-
-#BREW_LOCATION
-#APT_GET_LOCATION
 
 HOST_OS_VERSION=$(uname -s)
 HOST_OS=''
@@ -20,18 +16,33 @@ case $HOST_OS_VERSION in
    *) HOST_OS='other';;
 esac
 
+'''
+if [[ "$HOST_OS" = "windows" ]]; then
+  if [[ -f $CHOCO_LOCATION ]]; then
+    echo "Chocolatey found on host."
+  else 
+    echo "Chocolatey not found on host."
+    echo "Install Chocolatey manually."
+    # Install Choco
+elif [[ "$HOST_OS" = "mac" ]]; then
+  if [[ -f $BREW_LOCATION ]]; then
+    echo "Brew found on host."
+  else 
+    echo "Brew not found on host."
+    echo "Install Brew manually."
+    # Install Brew
+elif [[ "$HOST_OS" = "linux" ]]; then
+  if [[ -f $BREW_LOCATION ]]; then
+    echo "apt-get found on host."
+  else 
+    echo "apt-get not found on host."
+    echo "Install apt-get manually."
+    # Install apt-get
+fi
+'''
+
 if [[ -f $CMAKE_LOCATION ]]; then
   echo "CMake found on host."
-
-  if [[ "$HOST_OS" = "windows" ]]; then
-    if [[ -f $CHOCO_LOCATION ]]; then
-      echo "Chocolatey found on host."
-    else 
-      echo "Chocolatey not found on host."
-      echo "Install Chocolatey manually."
-      # Install Choco
-    fi
-
   elif [[ "$HOST_OS" = "other" ]];  then
     echo "Host OS not supported."
   fi
@@ -43,7 +54,7 @@ else
   elif [[ "$HOST_OS" = "mac" ]]; then
     brew install cmake
   elif [[ "$HOST_OS" = "linux" ]]; then
-    sudo apt-get -y install cmake
+    sudo apt-get install cmake
   elif [[ "$HOST_OS" = "other" ]];  then
     echo "Host OS not supported."
   fi
@@ -57,7 +68,7 @@ if [[ "${INSTALLED_VERSION_ARRAY[2]}" < "$MINIMUM_VERSION" ]]; then
   elif [[ "$HOST_OS" = "mac" ]]; then
     brew upgrade cmake -v
   elif [[ "$HOST_OS" = "linux" ]]; then
-    sudo apt-get -y update
+    sudo apt-get update
   elif [[ "$HOST_OS" = "other" ]];  then
     echo "Host OS not supported."
   fi
@@ -66,4 +77,4 @@ else
   echo "CMake version supported."
 fi
 
-sleep 3s
+#sleep 3s
