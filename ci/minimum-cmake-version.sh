3f07c5d17480fefe8f2802eb44f1e8f545ef7c41
Erik Tran <35762899+eriktran@users.noreply.github.com>
2021-09-21 14:49:37 +0200

    Update minimum-cmake-version.sh
    Added support for different operating systems to install and updates CMake automatically.
    
    Co-Authored-By: Clementine Jensen <gusjensecl@student.gu.se>

--- ci/minimum-cmake-version.sh
+++ ci/minimum-cmake-version.sh
@@ -5,17 +5,65 @@ MINIMUM_VERSION=3.12.0
 INSTALLED_VERSION_ARRAY=($INSTALLED_VERSION)
 
 CMAKE_LOCATION=$(where cmake)
+CHOCO_LOCATION=$(where choco)
+
+#BREW_LOCATION
+#APT_GET_LOCATION
+
+HOST_OS_VERSION=$(uname -s)
+HOST_OS=''
+
+case $HOST_OS_VERSION in
+   Darwin) HOST_OS='mac';;
+   Linux) HOST_OS='linux';;
+   CYGWIN*|MINGW32*|MSYS*|MINGW*) HOST_OS='windows';;
+   *) HOST_OS='other';;
+esac
 
 if [[ -f $CMAKE_LOCATION ]]; then
-  echo "hello"
-  sleep 5s
+  echo "CMake found on host."
+
+  if [[ "$HOST_OS" = "windows" ]]; then
+    if [[ -f $CHOCO_LOCATION ]]; then
+      echo "Chocolatey found on host."
+    else 
+      echo "Chocolatey not found on host."
+      echo "Install Chocolatey manually."
+      # Install Choco
+    fi
+
+  elif [[ "$HOST_OS" = "other" ]];  then
+    echo "Host OS not supported."
+  fi
+
+else 
+  echo "CMake not found on host."
+  if [[ "$HOST_OS" = "windows" ]]; then
+    choco install cmake
+  elif [[ "$HOST_OS" = "mac" ]]; then
+    brew install cmake
+  elif [[ "$HOST_OS" = "linux" ]]; then
+    sudo apt-get -y install cmake
+  elif [[ "$HOST_OS" = "other" ]];  then
+    echo "Host OS not supported."
+  fi
+
 fi
-
-
 
 if [[ "${INSTALLED_VERSION_ARRAY[2]}" < "$MINIMUM_VERSION" ]]; then
   echo "CMake version not supported. Minimum version required 3.12.0"
-  // update cmake
+  if [[ "$HOST_OS" = "windows" ]]; then
+    choco upgrade cmake
+  elif [[ "$HOST_OS" = "mac" ]]; then
+    brew upgrade cmake -v
+  elif [[ "$HOST_OS" = "linux" ]]; then
+    sudo apt-get -y update
+  elif [[ "$HOST_OS" = "other" ]];  then
+    echo "Host OS not supported."
+  fi
+  
+else
+  echo "CMake version supported."
 fi
 
-echo "CMake version supported."
+sleep 3s
