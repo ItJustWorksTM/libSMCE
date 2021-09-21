88e2116bd009c896a9cdbeea3b94b6345df0548d
Clementine Jensen <gusjensecl@student.gu.se>
2021-09-21 13:45:04 +0200

    Add test for checking if cmake is installed
    Checking if cmake is installed, so that we can install it if it is not found on host.
    
    Co-Authored-By: Erik Tran <35762899+eriktran@users.noreply.github.com>

--- ci/minimum-cmake-version.sh
+++ ci/minimum-cmake-version.sh
@@ -4,8 +4,18 @@ MINIMUM_VERSION=3.12.0
 
 INSTALLED_VERSION_ARRAY=($INSTALLED_VERSION)
 
+CMAKE_LOCATION=$(where cmake)
+
+if [[ -f $CMAKE_LOCATION ]]; then
+  echo "hello"
+  sleep 5s
+fi
+
+
+
 if [[ "${INSTALLED_VERSION_ARRAY[2]}" < "$MINIMUM_VERSION" ]]; then
   echo "CMake version not supported. Minimum version required 3.12.0"
+  // update cmake
 fi
 
 echo "CMake version supported."
