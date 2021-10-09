841d2faec9a62222d206d0762af89c6c7b3637ff
AeroStun <24841307+AeroStun@users.noreply.github.com>
2021-10-09 15:01:52 +0200

    [cmcov] Add basic rewrite rules to correctly scan runtime scripts
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>

--- ci/scan-cmake-commands.sh
+++ ci/scan-cmake-commands.sh
@@ -14,7 +14,13 @@ for f in $(find "${base_dir}" -type d \( -path "${base_dir}/build" -o -path "${b
   negative_report=$(jq -c --arg fname "$f" --argjson lines "${json_lno}" '.coverage += {($fname): $lines}' <<< "${negative_report}")
 done
 
-jq -c "select(has(\"version\") | not) | select(.file | startswith(\"${base_dir}/build\") | not) | select(.file | startswith(\"${base_dir}\")) | {\"file\": .file, \"line\": .line}" CMakeTrace/*.json \
+sed -e "s#${base_dir}/build/test/test_dir/manifests#${base_dir}/test/manifests#g" \
+    -e "s#${base_dir}/build/test/test_dir/smce_root/RtResources/SMCE/share/CMake/Scripts/ConfigureSketch.cmake#${base_dir}/CMake/Scripts/ConfigureSketch.cmake#g"\
+    -e "s#${base_dir}/build/test/test_dir/smce_root/RtResources/SMCE/share/CMake/Modules/BindGen.cmake#${base_dir}/CMake/Modules/BindGen.cmake#g"\
+    -e "s#${base_dir}/build/test/test_dir/smce_root/RtResources/SMCE/share/CMake/Modules/#${base_dir}/CMake/Runtime/#g"\
+    -e "s#${base_dir}/build/test/test_dir/smce_root/tmp/.*/CMakeLists.txt#${base_dir}/CMake/Runtime/CMakeLists.txt#g"\
+    CMakeTrace/*.json \
+| jq -c "select(has(\"version\") | not) | select(.file | startswith(\"${base_dir}/build\") | not) | select(.file | startswith(\"${base_dir}\")) | {\"file\": .file, \"line\": .line}" \
 | jq -nc --argjson init "${negative_report}" 'reduce inputs as $e ($init; .coverage[$e.file][$e.line | tostring] += 1)' \
 | sed "s#${base_dir}/##g" \
 > cmake-codecov-report.json
