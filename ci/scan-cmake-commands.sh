54f4f0d17b875bd6311e089a6c04bf4387e6b8e3
AeroStun <24841307+AeroStun@users.noreply.github.com>
2021-10-10 03:37:18 +0200

    [cmcov] Update filters to account for test manifests
    Those get copied twice
    
    Signed-off-by: AeroStun <24841307+AeroStun@users.noreply.github.com>

--- ci/scan-cmake-commands.sh
+++ ci/scan-cmake-commands.sh
@@ -14,7 +14,7 @@ for f in $(find "${base_dir}" -type d \( -path "${base_dir}/build" -o -path "${b
   negative_report=$(jq -c --arg fname "$f" --argjson lines "${json_lno}" '.coverage += {($fname): $lines}' <<< "${negative_report}")
 done
 
-sed -e "s#${base_dir}/build/test/test_dir/manifests#${base_dir}/test/manifests#g" \
+sed -e "s#${base_dir}/build/test/test_dir/smce_root/tmp/.*/manifests/#${base_dir}/test/manifests/#g" \
     -e "s#${base_dir}/build/test/test_dir/smce_root/RtResources/SMCE/share/CMake/Scripts/ConfigureSketch.cmake#${base_dir}/CMake/Scripts/ConfigureSketch.cmake#g"\
     -e "s#${base_dir}/build/test/test_dir/smce_root/RtResources/SMCE/share/CMake/Modules/BindGen.cmake#${base_dir}/CMake/Modules/BindGen.cmake#g"\
     -e "s#${base_dir}/build/test/test_dir/smce_root/RtResources/SMCE/share/CMake/Modules/#${base_dir}/CMake/Runtime/#g"\
