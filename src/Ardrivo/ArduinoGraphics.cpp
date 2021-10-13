f3590ab650c128b9070def7f7131dafb90d7ab8f
ziggyt <frmax@student.chalmers.se>
2021-10-13 20:26:54 +0200

    Add template ArduinoGraphics.cpp and ArduinoGraphics.h

---
+++ src/Ardrivo/ArduinoGraphics.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ziggy on 2021-10-13.
+//
+
+#include "ArduinoGraphics.h"
---
+++ src/Ardrivo/ArduinoGraphics.h
@@ -0,0 +1,8 @@
+//
+// Created by ziggy on 2021-10-13.
+//
+
+#ifndef LIBSMCE_ARDUINOGRAPHICS_H
+#define LIBSMCE_ARDUINOGRAPHICS_H
+
+#endif // LIBSMCE_ARDUINOGRAPHICS_H
