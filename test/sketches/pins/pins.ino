2455a83869fa5802787eb9b68af287e9dfef3743
tanyab <tanyab@student.chalmers.se>
2021-10-29 13:46:21 +0200

    Combining BoardView GPIO and BoardView GPIO Analog tests, combining BoardViewUART and BoardViewUART Write tests

--- test/sketches/pins/pins.ino
+++ test/sketches/pins/pins.ino
@@ -1,9 +1,12 @@
 void setup() {
     pinMode(0, INPUT);
     pinMode(2, OUTPUT);
+    pinMode(3, INPUT);
+    pinMode(4, OUTPUT);
 }
 
 void loop() {
     digitalWrite(2, !digitalRead(0));
+    analogWrite(4, analogRead(3));
     delay(1);
 }
