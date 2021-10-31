8170bb535b9950b00e3e60affb80e006bcc8bd3a
tanyab <tanyab@student.chalmers.se>
2021-10-31 09:52:40 +0100

    Remove commented out code in uart.ino and add else statements

--- test/sketches/uart/uart.ino
+++ test/sketches/uart/uart.ino
@@ -2,25 +2,6 @@ void setup() {
     Serial.begin(9600);
 }
 
-/*void loop() {
-    if(Serial.available() == 11)
-        Serial.print(Serial.readString());
-    if(Serial.available() == 12) {
-        const char expected[12] = "HELLO WORLD";
-        char toWrite[12];
-        if(Serial.availableForWrite() == 64) {
-            for (int i = 0; i < sizeof(expected); i++) {
-                if (Serial.peek() == expected[i]) {
-                    toWrite[i] = Serial.read();
-                }
-            }
-            for (int i = 0; i < sizeof(toWrite); i++){
-                Serial.print(toWrite[i]);
-            }
-        }
-    }
-}*/
-
 void loop() {
     if(Serial.available() == 11) {
         char expected[] = "HELLO";
@@ -41,5 +22,9 @@ void loop() {
         else if(Serial.peek() == '\0'){
             Serial.print(Serial.readString());
         }
+        else
+            Serial.print(Serial.readString());
     }
+    else
+        Serial.print(Serial.readString());
 }
